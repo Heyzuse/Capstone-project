d6d146eb767f450d8b1038990e35997ece006658
Heyzuse <dj1253105@gmail.com>
2023-07-28 15:08:03 -0500

    Built and better defined models and migrated

--- WorkoutApp/models.py
+++ WorkoutApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=10, choices=(('M', 'Male'), ('F', 'Female'), ('O', 'Other')))
+    fitness_goal = models.CharField(max_length=200)
+
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    duration = models.IntegerField(help_text="Duration in minutes")
+
+class ExerciseType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Exercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    type = models.ForeignKey(ExerciseType, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    repetitions = models.IntegerField()
+    sets = models.IntegerField()
