a532a1f67b5ad78aec3c192352fae768e31467db
Heyzuse <dj1253105@gmail.com>
2023-08-11 16:02:45 -0500

    Added admin panel models

--- WorkoutApp/admin.py
+++ WorkoutApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Exercise, Profile, Workout
 
-# Register your models here.
+admin.site.register(Exercise)
+admin.site.register(Profile)
+admin.site.register(Workout)
